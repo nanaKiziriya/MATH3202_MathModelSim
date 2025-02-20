2aa10b4cb99b8cf6e26ca13a1a37b79984676075
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2025-02-19 19:02:46 -0500

    Update MidSqLengthTester.java

--- RandomNumber/MiddleSquares/MidSqLengthTester.java
+++ RandomNumber/MiddleSquares/MidSqLengthTester.java
@@ -18,9 +18,9 @@ import java.util.ArrayList;
 class Main {
     
     // USER INPUTS
-    static int firstRoot=0, seedLength=2;
-    static boolean printTailOfEachSeed = false; // during calculations
-    static boolean printSeedsOfMaxTailL = true; // after calculations
+    static int firstRoot=514, seedLength=3;
+    static boolean printTailOfEachSeed = true; // during calculations
+    static boolean printSeedsOfMaxTailL = false; // after calculations
     static boolean printTailLOfEachSeed = false; // after calculations
     
     public static void main(String[] args) {
@@ -33,28 +33,33 @@ class Main {
         ArrayList<Integer> cycleHolder = new ArrayList<>(); // reset empty per seed, holds cycle in order, helps calc seed-tailL pair quicker
         HashSet<Integer> cycleChecker = new HashSet<Integer>(); // reset empty per seed, for quickly checking cycle
         int tailLength=0, maxTailLength=0;
-        int tempTail=0, tempIndex=0; // works with tempSeed, tempIndex instead of ArrayList.indexOf() for time efficiency
+        int tempTail=0, tempIndex=0, tailMin=0; // works with tempSeed, tempIndex instead of ArrayList.indexOf() for time efficiency
+        boolean cycleCheckerFlag, seedTailFlag;
         
         
         // efficient calc for each root
         for(int i=firstRoot, j=i;i<=lastRoot;i++){
             // reset vals/prep for next root
-            j=i; tailLength=0; tempIndex=0;
+            j=i; tailLength=tempIndex=0;
             cycleChecker.clear();
             cycleHolder.clear();
+            cycleCheckerFlag=seedTailFlag=false;
             
             while(true){ // for each root, continues sequ calc until hits repeat in either root's cycleChecker or rootChecker
-                if(!(cycleChecker.contains(j)||seedTail.keySet().contains(j))){
+                cycleCheckerFlag=cycleChecker.contains(j);
+                seedTailFlag=seedTail.keySet().contains(j);
+                if(!(cycleCheckerFlag||seedTailFlag)){
                     /*printTailOfEachSeed*/
                     if(printTailOfEachSeed) System.out.print(j+" ");
                     cycleChecker.add(j);
                     cycleHolder.add(j);
                     tailLength++;
                 } else{
-                    if(seedTail.keySet().contains(j)) tailLength+=seedTail.get(j);
+                    // if cycleChecker is flagged (instead of seedTail.keySet()) that means its a NEW periodic point, thus every subsequent element has the same period/tailLength, and the tempTail of the element that flagged cycleChecker is the sharp lower bound for tailLengths of all elements in the current orbit
+                    if(seedTailFlag) tailLength+=seedTail.get(j);
                     /*printTailOfEachSeed*/
                     if(printTailOfEachSeed) System.out.print(j+"... ");
-                    break; // break this root sequ
+                    break; // break this seed sequ
                 }
                 j=fnct(j);
             }
@@ -64,9 +69,11 @@ class Main {
             if(printTailOfEachSeed) System.out.println("["+tailLength+"]");
             
             // update tailSeed, seedTail, time efficient
+            // NOTE: If a non-fixed periodic point is hit (flagged cycleChecker in loop) every subsequent element has the exact same period/tailLength
+            tailMin = (cycleCheckerFlag)? tailLength-cycleHolder.indexOf(j):0;
             for(int tempSeed:cycleHolder){
                 if(!seedTail.keySet().contains(tempSeed)){ // if this seed hasn't been calculated and stored b4
-                    tempTail = tailLength-tempIndex;
+                    tempTail = Math.max(tailLength-tempIndex,tailMin);
                     seedTail.put(tempSeed,tempTail);
                     if(!tailSeed.keySet().contains(tempTail)) tailSeed.put(tempTail,new TreeSet<>());
                     tailSeed.get(tempTail).add(tempSeed);
@@ -76,6 +83,8 @@ class Main {
             
             // update maxTailLength
             if(maxTailLength<tailLength) maxTailLength=tailLength;
+            
+            // for loop again
         }
         
         // END
